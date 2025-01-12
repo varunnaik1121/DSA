3b5539b5515c031c500a43b1992dbb1d90a51ef7
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2025-01-12 12:17:10 +0530

    Time: 17 ms (36.80%) | Memory: 34.8 MB (8.88%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        if(s.size()%2!=0) return false;
+        stack<int> locked_st;
+        stack<int> unlocked_st;
+        for(int i=0;i<s.size();i++){
+            if(locked[i]=='1'){
+                if(s[i]=='('){
+                    locked_st.push(i);
+                }else{
+                    if(locked_st.size()>0 && s[locked_st.top()]=='('){
+                        locked_st.pop();
+                    }else{
+                        if(unlocked_st.size()>0){
+                            unlocked_st.pop();
+                        }else{
+                            return false;
+                        }
+                    }
+                }
+            }else{
+                unlocked_st.push(i);
+            }
+        }
+        
+        while(locked_st.size()>0 && unlocked_st.size()>0){
+            if(locked_st.top()<unlocked_st.top()){
+                locked_st.pop();unlocked_st.pop();
+            }else{
+                return false;
+            }
+        }
+        if(locked_st.size()>0) return false;
+        if(unlocked_st.size()%2==0) return true;
+        return false;
+    }
+};
