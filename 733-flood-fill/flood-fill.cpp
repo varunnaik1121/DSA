d69a739d18c188dc39bb27b36dd036afa1fc76fe
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2025-01-20 13:53:53 +0530

    Time: 2 ms (18.66%) | Memory: 18.1 MB (54.55%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -5,36 +5,26 @@ public:
         int startColor=image[sr][sc];
         int n=image.size();
         int m=image[0].size();
-        vector<vector<int>> temp(n,vector<int> (m));
-        for(int i=0;i<image.size();i++){
-            for(int j=0;j<m;j++){
-                temp[i][j]=image[i][j];
-            }
-        }
-    //   if(image[sr][sc]==1){
-          q.push({sr,sc});
-    //   }
-        // int n=image.size();
-        // int m=image[0].size();
+        q.push({sr,sc});
         while(!q.empty()){
             auto it=q.front();
             q.pop();
             int row=it.first;
             int col=it.second;
-            temp[row][col]=color;
+            image[row][col]=color;
             int delRow[]={+1,-1,0,0};
             int delCol[]={0,0,+1,-1};
             for(int i=0;i<4;i++){
                 int tRow=row+delRow[i];
                 int tCol=col+delCol[i];
-                if(tRow>=0 && tRow<n && tCol>=0 && tCol<m && temp[tRow][tCol]!=color && temp[tRow][tCol]==startColor){
+                if(tRow>=0 && tRow<n && tCol>=0 && tCol<m && image[tRow][tCol]!=color && image[tRow][tCol]==startColor){
                      image[tRow][tCol]=color;
                      q.push({tRow,tCol});
                 }
             }
             
         }
-        return temp;
+        return image;
 
     }
 };
