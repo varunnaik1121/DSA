c4ae09cfdef11959267ba034bde5ca751efbc41f
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2025-02-12 11:43:39 +0530

    Time: 21 ms (68.07%) | Memory: 63.8 MB (87.26%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -10,23 +10,18 @@ private:
     }
 public:
     int maximumSum(vector<int>& nums) {
-        map<int,priority_queue<int,vector<int>,greater<int>>> mpp;
-        for(int i=0;i<nums.size();i++){
-            int sum=sumOfDigits(nums[i]);
-            mpp[sum].push(nums[i]);
-            if(mpp[sum].size()>2){
-                mpp[sum].pop();
+       unordered_map<int,int> mpp;
+       int maxi=-1;
+       for(auto num:nums){
+            int digitSum=sumOfDigits(num);
+            if(mpp.find(digitSum)!=mpp.end()){
+                maxi=max(maxi,num+mpp[digitSum]);
+                mpp[digitSum]=max(num,mpp[digitSum]);
+            }else{
+                mpp[digitSum]=num;
             }
-        }
-        int maxi=-1;
-        for(auto it:mpp ){
-            if(it.second.size()>=2){
-               int num1=it.second.top();it.second.pop();
-               int num2=it.second.top();it.second.pop();
-               maxi=max(maxi,num1+num2);
-            }
-        }
-        return maxi;
+       }
+       return maxi;
 
         
     }
