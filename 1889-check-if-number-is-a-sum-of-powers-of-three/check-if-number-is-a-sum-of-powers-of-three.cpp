153b0f2879cdf476617c2b87686ac82044a843fa
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2025-03-11 12:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (92.70%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,18 +1,15 @@
 class Solution {
-    bool helper(int i,int total,int n){
-        if(total==n) return true;
-        if(pow(3,i)+total>n) return false;
-        if(helper(i+1,total+pow(3,i),n)){
-            return true;
-        } 
-        if(helper(i+1,total,n)){
-            return true;
-        }
-        return false;
-    }
+   
 public:
     bool checkPowersOfThree(int n) {
-        return helper(-1,0,n);
+        while(n>0){
+            int rem=n%3;
+            if(rem!=0 && rem!=1){
+                return false;
+            }
+            n/=3;
+        }
+        return true;
     }
 
 };
