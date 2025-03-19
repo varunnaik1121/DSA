aec4cc2caf9c799ba35fedf39d7cbc5bdc0e7274
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2025-03-19 22:37:18 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (8.42%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int posCount=0;
-        int negCount=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>0){
-                posCount++;
-
-            }else if(nums[i]<0){
-                negCount++;
+        int n=nums.size();
+        int start=0,end=n-1;
+        int minPosIdx=-1;
+        int minNegIdx=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]>0){
+                minPosIdx=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
         }
-        return max(posCount,negCount);
+        start=0,end=n-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]<0){
+                minNegIdx=mid;
+                start=mid+1;
+
+            }else{
+                end=mid-1;
+            }
+        }
+        cout<<minNegIdx<<" "<< minPosIdx<<endl;
+        if(minPosIdx==-1) return minNegIdx+1;
+        if(minNegIdx==-1) return n-minPosIdx;
+        return max(minNegIdx+1,n-minPosIdx);
+
     }
 };
