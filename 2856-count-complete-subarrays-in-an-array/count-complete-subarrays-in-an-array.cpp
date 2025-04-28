5666e74bc8d264e58c95318dc7e90d9093494c7c
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2025-04-28 22:57:07 +0530

    Time: 23 ms (52.54%) | Memory: 41.8 MB (75.75%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        //find the distinct element in the whole array and then mathc that 
-
-        //
         unordered_set<int> st(nums.begin(),nums.end());
+        unordered_map<int,int> mpp;
+        int i=0,j=0;
         int n=nums.size();
+        int uniqCount=0;
         int ans=0;
-        for(int i=0;i<n;i++){
-            unordered_set<int> temp;
-            for(int j=i;j<n;j++){
-                temp.insert(nums[j]);
-                if(temp.size()==st.size()){
-                    ans++;
-                }
+        while(i<n && j<n){
+            mpp[nums[j]]++;
+            if(mpp[nums[j]]==1){
+                uniqCount++;
             }
+            while(uniqCount==st.size()){
+                ans+=n-j;
+                mpp[nums[i]]--;
+                if(mpp[nums[i]]==0){
+                    uniqCount--;
+                }
+                i++;
+            }
+            j++;
         }
         return ans;
-
+        
     }
 };
