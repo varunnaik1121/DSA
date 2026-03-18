18d2c641a10ebf24582bc6aa96ff21bf249c1bab
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2026-03-18 10:51:52 +0530

    Time: 47 ms (6.24%) | Memory: 23.9 MB (19.55%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,12 +5,12 @@ public:
         int end=0;
         int n=nums.size();
        while(start<n && end<n){
-            while(start<n && nums[start]!=0){
+            while(start<n && end<n && nums[start]!=0){
                 start++;
 
             }
             end=start+1;
-            while(end<n && nums[end]==0) end++;
+            while(start<n && end<n && nums[end]==0) end++;
             if(start<n && end<n){
                 swap(nums[start],nums[end]);
             }
