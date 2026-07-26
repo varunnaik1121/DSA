b1d8ebcc8ec2a8d6aee416e37e11785dbacef719
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2026-07-26 19:24:49 +0530

    Time: 1 ms (38.13%) | Memory: 29.1 MB (11.68%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -11,29 +11,16 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p,TreeNode* q){
-        if(!p && !q) return true;
-        if(!p && q) return false;
-        if(!q && p) return false;
-        return (p->val==q->val) && (isSameTree(p->left,q->left)) && (isSameTree(p->right,q->right));
-    }
-    void helper(TreeNode* root,TreeNode* subRoot,bool &isSame){
-        if(isSame) return;
-        if(!root) return;
-        helper(root->left,subRoot,isSame);
-        if(root->val==subRoot->val){
-            if(isSameTree(root,subRoot)){
-                isSame=true;
-                return;
-            }
-        }
-        helper(root->right,subRoot,isSame);
+     bool isSametree(TreeNode* a, TreeNode* b) {
+        if(!a && !b) return true;
+        if(!a || !b) return false;
+
+        if(a -> val != b -> val) return false; //value mismatch
+        return isSametree(a -> left,b -> left) && isSametree(a -> right,b -> right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        //start with visiting every node and check
-        //when u find the root node then pass both the trees for the testing
-       bool isSame=false; 
-        helper(root,subRoot,isSame);
-       return isSame; 
+        if(!root) return false;
+        if(isSametree(root,subRoot)) return true;
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
     }
 };
