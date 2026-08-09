f94f6916bbe8de1ff79e95c25a4d1de9481ff24b
Varun Naik <92010950+varunnaik1121@users.noreply.github.com>
2026-08-09 23:33:31 +0530

    Time: 2 ms (7.94%) | Memory: 25.2 MB (3.90%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -12,15 +12,42 @@
 class Solution {
 public:
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        if(!root){
-            return NULL;
-        }
-        
-        root->left=removeLeafNodes(root->left,target);
-        root->right=removeLeafNodes(root->right,target);
-        if(root->val==target && !root->left && !root->right){
-            return NULL;
+        if(!root) return root;
+        stack<TreeNode*> st;
+        st.push(root);
+        unordered_map<TreeNode*,TreeNode*> parentMap;
+        set<TreeNode*> visited;
+        while(!st.empty()){
+            TreeNode* node=st.top();
+            st.pop();
+            //if its a leaf node then
+            if(!node->left && !node->right){
+                if(node->val==target){
+                    if(parentMap.find(node)==parentMap.end()){
+                        return NULL;
+                    }else{
+                        TreeNode* parentNode=parentMap[node];
+                        if(parentNode->left==node){
+                            parentNode->left=NULL;
+                        }else{
+                            parentNode->right=NULL;
+                        }
+                    }
+                }
+            }else if(visited.find(node)==visited.end()){
+                st.push(node);
+                visited.insert(node);
+                if(node->left){
+                    st.push(node->left);
+                    parentMap[node->left]=node;
+                }
+                if(node->right){
+                    st.push(node->right);
+                    parentMap[node->right]=node;
+                }
+            }
         }
         return root;
+
     }
 };
